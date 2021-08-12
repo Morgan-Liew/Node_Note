6305968c2fb81b95bfdcc009ee5085647be03f08
Morgan.Liew <morgan_liew@qq.com>
2021-08-12 17:11:55 +0800

    update the record of node and add the test

---
+++ node/math.js
@@ -0,0 +1,5 @@
+/* 
+    定义一个math模块
+*/
+exports.add = (a,b) => ( a + b );
+exports.mul = (a,b) => ( a * b );
---
+++ node/module.js
@@ -0,0 +1,8 @@
+let math = require('./math')
+
+console.log(math.add(2,3));
+console.log(math.mul(2,3));
+
+// exports const a = 15
+
+console.log(arguments.callee + '')
